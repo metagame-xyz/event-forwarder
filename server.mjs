53799dcc9c9dc6f25be21c6f16439a45a1508691
Brenner Spear <blspear@gmail.com>
2021-10-20 16:01:50 -0500

    pretty much done

--- server.mjs
+++ server.mjs
@@ -1,10 +1,13 @@
 import Web3 from 'web3';
 import fetch from 'node-fetch-retry';
+import { createHmac } from 'crypto';
 
 const NETWORK = process.env.NETWORK.toLowerCase();
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const BIRTHBLOCK_WEBHOOK_URL = process.env.BIRTHBLOCK_WEBHOOK_URL;
+const EVENT_FORWARDER_AUTH_TOKEN = process.env.EVENT_FORWARDER_AUTH_TOKEN;
 
 const infuraNetworkString = NETWORK == 'ethereum' ? 'mainnet' : `${NETWORK}`;
 const etherscanNetworkString = NETWORK == 'ethereum' ? '' : `-${NETWORK}`;
@@ -12,7 +15,7 @@ const providerURL = `wss://${infuraNetworkString}.infura.io/ws/v3/${INFURA_API_K
 
 const web3 = new Web3(providerURL);
 
-const fetchOptions = {
+const fetchBaseOptions = {
     retry: 12,
     pause: 2000,
     callback: (retry) => {
@@ -20,22 +23,48 @@ const fetchOptions = {
     },
 };
 
-const fetcher = (url) => fetch(url, fetchOptions).then((r) => r.json());
+const signMessage = (body) => {
+    const hmac = createHmac('sha256', EVENT_FORWARDER_AUTH_TOKEN); // Create a HMAC SHA256 hash using the auth token
+    hmac.update(JSON.stringify(body), 'utf8'); // Update the token hash with the request body using utf8
+    const digest = hmac.digest('hex');
+    return digest;
+};
+
+const fetcher = (url, options) => fetch(url, options).then((r) => r.json());
 
 const getContractAbi = async (contractAddress) =>
     await fetcher(
         `https://api${etherscanNetworkString}.etherscan.io/api?module=contract&action=getabi&address=${contractAddress}&apikey=${ETHERSCAN_API_KEY}`,
+        fetchBaseOptions,
     );
 
 const { result: contractAbi } = await getContractAbi(CONTRACT_ADDRESS);
 
 const contract = new web3.eth.Contract(JSON.parse(contractAbi), CONTRACT_ADDRESS);
 
+const webhookOptions = (body) => ({
+    ...fetchBaseOptions,
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: {
+        'Content-Type': 'application/json',
+        'x-Event-Forwarder-Signature': signMessage(body),
+    },
+});
+
 contract.events
     .Mint()
-    .on('data', (event) => {
-        console.log('new minter:', event.returnValues[0]);
-        console.log('tokenId:', event.returnValues[1]);
+    .on('data', async (event) => {
+        const minterAddress = event.returnValues[0];
+        const tokenId = event.returnValues[1];
+
+        const body = { minterAddress, tokenId };
+
+        console.log('body:', body);
+
+        const data = await fetcher(BIRTHBLOCK_WEBHOOK_URL, webhookOptions(body));
+
+        console.log('data:', data);
     })
     .on('error', (error) => {
         console.log('error:', error);
