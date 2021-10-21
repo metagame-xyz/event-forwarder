ffaa829020cb4e3864077528b4712724bc5b9866
Brenner Spear <blspear@gmail.com>
2021-10-21 12:26:17 -0500

    yay all clean

--- server.mjs
+++ server.mjs
@@ -1,66 +1,20 @@
-import Web3 from 'web3';
 import fetch from 'node-fetch-retry';
 import { createHmac } from 'crypto';
 import { createAlchemyWeb3 } from '@alch/alchemy-web3';
 
 const NETWORK = process.env.NETWORK.toLowerCase();
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 const BIRTHBLOCK_WEBHOOK_URL = process.env.BIRTHBLOCK_WEBHOOK_URL;
 const EVENT_FORWARDER_AUTH_TOKEN = process.env.EVENT_FORWARDER_AUTH_TOKEN;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
-const infuraNetworkString = NETWORK == 'ethereum' ? 'mainnet' : `${NETWORK}`;
 const alchemyNetworkString = NETWORK == 'ethereum' ? 'mainnet' : `${NETWORK}`;
 const etherscanNetworkString = NETWORK == 'ethereum' ? '' : `-${NETWORK}`;
-const providerURL = `wss://${infuraNetworkString}.infura.io/ws/v3/${INFURA_API_KEY}`;
 
-// const web3 = new Web3();
 const web3 = createAlchemyWeb3(
     `wss://eth-${alchemyNetworkString}.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
 );
-
-console.log(`wss://eth-${alchemyNetworkString}.alchemyapi.io/v2/${ALCHEMY_API_KEY}`);
-
-// const debug = (...messages) => console.log(...messages);
-// /**
-//  * Refreshes provider instance and attaches even handlers to it
-//  */
-// function refreshProvider(web3Obj, providerUrl) {
-//     let retries = 0;
-
-//     function retry(event) {
-//         if (event) {
-//             debug(['event', event]);
-//             debug('Web3 provider disconnected or errored.');
-//             retries += 1;
-
-//             if (retries > 5) {
-//                 debug(`Max retries of 5 exceeding: ${retries} times tried`);
-//                 return setTimeout(refreshProvider, 5000);
-//             }
-//         } else {
-//             debug(`Reconnecting web3 provider`);
-//             refreshProvider(web3Obj, providerUrl);
-//         }
-
-//         return null;
-//     }
-
-//     const provider = new Web3.providers.WebsocketProvider(providerUrl);
-
-//     provider.on('end', (e) => retry(e));
-//     provider.on('error', (e) => retry(e));
-
-//     web3Obj.setProvider(provider);
-
-//     debug('New Web3 provider initiated');
-
-//     return provider;
-// }
-
-// refreshProvider(web3, providerURL);
 
 const fetchBaseOptions = {
     retry: 12,
@@ -94,8 +48,8 @@ const webhookOptions = (body) => ({
     method: 'post',
     body: JSON.stringify(body),
     headers: {
-        'Content-Type': 'application/json',
-        'x-Event-Forwarder-Signature': signMessage(body),
+        'content-type': 'application/json',
+        'x-event-forwarder-signature': signMessage(body),
     },
 });
 
@@ -113,9 +67,17 @@ contract.events
             tokenId: event.returnValues[1],
         };
 
-        const data = await fetcher(BIRTHBLOCK_WEBHOOK_URL, webhookOptions(body));
+        console.log(`${minterAddress} minted tokenId ${tokenId}`);
 
-        console.log('data:', data);
+        const result = await fetcher(BIRTHBLOCK_WEBHOOK_URL, webhookOptions(body));
+
+        if (result.error) {
+            console.error(error);
+        } else {
+            console.log(
+                `${result.minterAddress} with   tokenId ${tokenId} has been added or updated`,
+            );
+        }
     })
     .on('error', (error) => {
         console.log('error:', error);
